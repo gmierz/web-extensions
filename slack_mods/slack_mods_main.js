2cf9cb8e2c9833c9c71c93cca18a58769ad95d56
gmierz <gmierz1@live.ca>
2019-09-24 11:40:11 -0400

    Change background color for directed messages to dark blue.

--- slack_mods/slack_mods_main.js
+++ slack_mods/slack_mods_main.js
@@ -5,26 +5,29 @@
 	to search through Slack for your discussions.
 */
 
-const dark_blue = "#C0DBFF"; // Not used yet.
-
+const dark_blue = "#C0DBFF";
 const light_blue = "#E2EDFF";
+
 const username = "sparky";
+const allusers = "@here"
 
 var checkForMessages = setInterval(function () {
 	var messages = document.querySelectorAll('div .c-virtual_list__item');
 
 	// Change the background of any messages which contain the username
 	messages.forEach(function(el) {
-		var found = el.innerText.includes(username) || el.innerText.includes('@' + username);
-		if (found) {
+		var found_fromusermsg = el.innerText.includes(username);
+		var found_tousermsg = el.innerText.includes('@' + username) || el.innerText.includes(allusers);
+		if (found_fromusermsg || found_tousermsg) {
 			var style = el.getAttribute('style');
-			if (style.includes('background: ' + light_blue)) {
+			var color = found_tousermsg ? dark_blue : light_blue;
+			if (style.includes('background: ' + color)) {
 				return;
 			}
 
 			el.setAttribute(
 				'style',
-				style + ' background: ' + light_blue + ';'
+				style + ' background: ' + color + ';'
 			);
 		}
 	});
